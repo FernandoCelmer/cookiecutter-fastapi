48b9c5cc2af2a33c699056be2e4a0c2c4d2272cd
FernandoCelmer <email@fernandocelmer.com>
2023-11-27 23:52:28 -0300

    ⚙️ FEATURE: Test Jinja

--- {{cookiecutter.project_slug}}/api/v1/endpoints/item.py
+++ {{cookiecutter.project_slug}}/api/v1/endpoints/item.py
@@ -6,11 +6,8 @@ router = APIRouter()
 @router.get("/item")
 {%- if cookiecutter.use_async == 'y' %}
 async def get():
-    return {"resource": "item"}
 {%- endif %}
-
-@router.get("/item")
 {%- if cookiecutter.use_async == 'ns' %}
 def get():
-    return {"resource": "item"}
 {%- endif %}
+    return {"resource": "item"}
