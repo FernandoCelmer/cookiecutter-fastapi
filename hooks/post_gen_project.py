25b6178f9d3c26cc5f00283ea73c7926e786d2fb
FernandoCelmer <email@fernandocelmer.com>
2024-01-03 22:55:09 -0300

    ⚙️ FEATURE: Initial Templates configuration

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,14 +3,26 @@ from pathlib import Path
 from shutil import rmtree
 
 
+CONTEXT = Path.cwd()
+
+
 def remove_mkdocs():
+    context_path = CONTEXT.joinpath('docs')
     remove('mkdocs.yml')
-    rmtree(Path.cwd().joinpath('docs'))
+    rmtree(context_path)
+
+
+def remove_templates():
+    context_path = CONTEXT.joinpath('app', 'static')
+    rmtree(context_path)
 
 
 def main():
   if "{{ cookiecutter.use_mkdocs }}" != "y":
         remove_mkdocs()
+
+  if "{{ cookiecutter.use_templates }}" != "y":
+        remove_templates()
 
 
 if __name__ == "__main__":
