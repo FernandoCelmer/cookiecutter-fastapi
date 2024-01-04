1c252288260cf2fcf3754e7e0439edd905ba2374
FernandoCelmer <email@fernandocelmer.com>
2024-01-03 22:29:53 -0300

    ⚙️ FEATURE: Inclusion of rule for Model and Schema ID type

--- {{cookiecutter.project_slug}}/app/models/item.py
+++ {{cookiecutter.project_slug}}/app/models/item.py
@@ -23,6 +23,7 @@ class Item(Base):
     __tablename__ = "item"
     __table_args__ = {'extend_existing': True}
 
+
 {%- if cookiecutter.id_type == 'UUID' %}
     id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
 {%- endif %}
