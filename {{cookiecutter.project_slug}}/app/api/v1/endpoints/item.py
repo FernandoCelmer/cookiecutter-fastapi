ceb8f24d7f85f3228ba92483cc0c74f95b12cbb9
Fernando Celmer <email@fernandocelmer.com>
2025-11-15 16:57:05 -0300

    🪲 BUG: Fix Jinja2 template syntax in item.py to use else instead of elif

--- {{cookiecutter.project_slug}}/app/api/v1/endpoints/item.py
+++ {{cookiecutter.project_slug}}/app/api/v1/endpoints/item.py
@@ -15,20 +15,20 @@ router = APIRouter()
 
 
 @router.get("", summary="Get items", response_description="List of items")
-{% if cookiecutter.use_async == 'y' %}
+{%- if cookiecutter.use_async == 'y' %}
 async def get_items({% if cookiecutter.use_auth == 'y' %}auth=Depends(authorization){% endif %}):
-{% elif cookiecutter.use_async == 'n' %}
+{%- else %}
 def get_items({% if cookiecutter.use_auth == 'y' %}auth=Depends(authorization){% endif %}):
-{% endif %}
+{%- endif %}
     """Get all items."""
     return {"resource": "item"}
 
 
 @router.get("/{item_id}", summary="Get item by ID")
-{% if cookiecutter.use_async == 'y' %}
+{%- if cookiecutter.use_async == 'y' %}
 async def get_item(item_id: {% if cookiecutter.id_type == 'UUID' %}UUID{% elif cookiecutter.id_type == 'Integer' %}int{% endif %}{% if cookiecutter.use_auth == 'y' %}, auth=Depends(authorization){% endif %}):
-{% elif cookiecutter.use_async == 'n' %}
+{%- else %}
 def get_item(item_id: {% if cookiecutter.id_type == 'UUID' %}UUID{% elif cookiecutter.id_type == 'Integer' %}int{% endif %}{% if cookiecutter.use_auth == 'y' %}, auth=Depends(authorization){% endif %}):
-{% endif %}
+{%- endif %}
     """Get a specific item by ID."""
     return {"item_id": item_id, "resource": "item"}
