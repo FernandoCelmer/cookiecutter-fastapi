c9a42d03b40a6c748c3d0909d35863dcbac9a0b5
Fernando Celmer <email@fernandocelmer.com>
2025-11-15 16:54:45 -0300

    🪲 BUG: Fix indentation error in item.py with async and integer id_type

--- {{cookiecutter.project_slug}}/app/api/v1/endpoints/item.py
+++ {{cookiecutter.project_slug}}/app/api/v1/endpoints/item.py
@@ -2,34 +2,33 @@
 Item endpoints.
 """
 
-from uuid import UUID
-
 from fastapi import APIRouter, Depends
 
 {%- if cookiecutter.use_auth == 'y' %}
 from app.core.auth.security import authorization
 {%- endif %}
 {%- if cookiecutter.id_type == 'UUID' %}
+from uuid import UUID
 {%- endif %}
 
 router = APIRouter()
 
 
 @router.get("", summary="Get items", response_description="List of items")
-{%- if cookiecutter.use_async == 'y' %}
+{% if cookiecutter.use_async == 'y' %}
 async def get_items({% if cookiecutter.use_auth == 'y' %}auth=Depends(authorization){% endif %}):
-{%- elif cookiecutter.use_async == 'n' %}
+{% elif cookiecutter.use_async == 'n' %}
 def get_items({% if cookiecutter.use_auth == 'y' %}auth=Depends(authorization){% endif %}):
-{%- endif %}
+{% endif %}
     """Get all items."""
     return {"resource": "item"}
 
 
 @router.get("/{item_id}", summary="Get item by ID")
-{%- if cookiecutter.use_async == 'y' %}
+{% if cookiecutter.use_async == 'y' %}
 async def get_item(item_id: {% if cookiecutter.id_type == 'UUID' %}UUID{% elif cookiecutter.id_type == 'Integer' %}int{% endif %}{% if cookiecutter.use_auth == 'y' %}, auth=Depends(authorization){% endif %}):
-{%- elif cookiecutter.use_async == 'n' %}
+{% elif cookiecutter.use_async == 'n' %}
 def get_item(item_id: {% if cookiecutter.id_type == 'UUID' %}UUID{% elif cookiecutter.id_type == 'Integer' %}int{% endif %}{% if cookiecutter.use_auth == 'y' %}, auth=Depends(authorization){% endif %}):
-{%- endif %}
+{% endif %}
     """Get a specific item by ID."""
     return {"item_id": item_id, "resource": "item"}
